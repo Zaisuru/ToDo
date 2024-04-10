787f7d561fad646b77c2005acca4b64706d85088
Thomas HOLDERBAUM <thomas.holderbaum@regnology.net>
2024-04-10 11:43:21 +0200

    add showItem function

--- scripts/addItems.js
+++ scripts/addItems.js
@@ -1,11 +1,25 @@
-//document.getElementById("addBtn").addEventListener("click", function() {
-  //  alert("Hello");
-//});
-const modalContainer = document.querySelector(".modal-container");
+// Display-Hide modal
+const modalContainer= document.querySelector(".modal-container");
 const modalTrigger = document.querySelectorAll(".modal-trigger");
+const btnClose = document.querySelector(".modal__btn-Close");
 
-modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal));
+modalTrigger.forEach( trigger => trigger.addEventListener("click", displayModal));
 
-function toggleModal(){
+btnClose.addEventListener("click", hideModal);
+function displayModal(){
     modalContainer.classList.toggle("active");
+}
+function hideModal(){
+    modalContainer.classList.remove("active");
+}
+// Create Item
+const btnAdd = document.querySelector(".modal__btn-Add");
+const itemsTitle= document.querySelector(".items__Title");
+
+btnAdd.addEventListener("click", addItems);
+
+function addItems(){
+    console.log(itemsTitle.value);
+
+    hideModal();
 }
---
+++ scripts/showItems.js
@@ -0,0 +1,40 @@
+//require json
+// Create Dom 
+const toDoContainer = document.getElementById("toDo__Container");
+const containerRow = document.createElement("div");
+const formCheck = document.createElement("div");
+const formInput = document.createElement("input");
+const formLabel = document.createElement("label");
+const formIcon = document.createElement("div");
+const linkEdit = document.createElement("a");
+const iconEdit = document.createElement("i");
+const linkTrash = document.createElement("a");
+const iconTrash = document.createElement("i");
+
+//add Class
+containerRow.classList.add("todo__Container_row");
+formCheck.classList.add("form-check");
+formInput.classList.add("form-check-input");
+formLabel.classList.add("form-check-label");
+formIcon.classList.add("form-icon");
+linkEdit.classList.add("fa__form_icon");
+iconEdit.classList.add("fa-solid", "fa-pen-to-square")
+linkTrash.classList.add("fa__form_icon", "fa__form_icon_trash");
+iconTrash.classList.add("fa-solid", "fa-trash");
+
+// Add attribute
+formInput.setAttribute("type", "checkbox");
+linkEdit.setAttribute("href", "#");
+linkTrash.setAttribute("href","#")
+formLabel.textContent= "Salut";
+
+// Display items
+toDoContainer.appendChild(containerRow);
+containerRow.appendChild(formCheck);
+formCheck.appendChild(formInput);
+formCheck.appendChild(formLabel);
+containerRow.appendChild(formIcon);
+formIcon.appendChild(linkEdit);
+linkEdit.appendChild(iconEdit);
+formIcon.appendChild(linkTrash);
+linkTrash.appendChild(iconTrash);
